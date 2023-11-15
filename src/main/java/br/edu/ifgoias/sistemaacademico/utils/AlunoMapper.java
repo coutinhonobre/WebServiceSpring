0ddd8eddc7658ec5097b09f9a7adadc322ad88ba
Coutinho Nobre <coutinhonobre360@outlook.com>
2023-11-15 00:39:30 -0300

    issue sonar

--- src/main/java/br/edu/ifgoias/sistemaacademico/utils/AlunoMapper.java
+++ src/main/java/br/edu/ifgoias/sistemaacademico/utils/AlunoMapper.java
@@ -8,20 +8,20 @@ public class AlunoMapper implements GenericMapper<Aluno, AlunoDTO> {
     @Override
     public Aluno convertDTOParaEntidade(AlunoDTO dto) {
         return new Aluno(
-                dto.getIdaluno(),
-                dto.getNome(),
-                dto.getSexo(),
-                dto.getDtNasc()
+            dto.getIdaluno(),
+            dto.getNome(),
+            dto.getSexo(),
+            dto.getDtNasc()
         );
     }
 
     @Override
     public AlunoDTO convertEntidadeParaDTO(Aluno aluno) {
         return new AlunoDTO(
-                aluno.getIdaluno(),
-                aluno.getNome(),
-                aluno.getSexo(),
-                aluno.getDtNasc()
+            aluno.getIdaluno(),
+            aluno.getNome(),
+            aluno.getSexo(),
+            aluno.getDtNasc()
         );
     }
 
